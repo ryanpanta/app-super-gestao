23e3451d1d1fcf8f0f332b537fa8a448148450ee
Ryan Rodrigues <ryan.rodrigues@grupoeduqi.com.br>
2024-11-26 14:11:33 -0300

    atualizações

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function Contato(){
-        return view('site.contato');
+        return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,5 +1,7 @@
 @isset($fornecedores)
     @foreach ($fornecedores as $indice => $fornecedor)
+        
+        <br>
         Fornecedor: {{ $fornecedor['nome'] }}
         <br>
         Status: {{ $fornecedor['status'] }}
@@ -9,5 +11,13 @@
         DDD: {{ $fornecedor['ddd'] ?? '' }}
         <br>
         Telefone: {{ $fornecedor['telefone'] ?? ''}}
+        @if ($loop->first)
+            <p>Primeira iteração do loop.</p>
+        @endif
+
+        @if ($loop->last)
+            <p>Última iteração do loop.</p>
+        @endif
+        <hr>
     @endforeach
 @endisset
