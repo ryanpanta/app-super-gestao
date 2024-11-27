fc2b89e36d77da142f799f008d5460dfefe9deab
Ryan Rodrigues <ryan.rodrigues@grupoeduqi.com.br>
2024-11-27 14:10:34 -0300

    atualizações

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function Contato(){
+    public function Contato(Request $request){
+        dump($request);
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
