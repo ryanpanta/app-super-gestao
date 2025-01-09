d0d6c04190b5eaace7c94de53ee099831c751704
Ryan Rodrigues <ryan.rodrigues@grupoeduqi.com.br>
2025-01-09 13:49:13 -0300

    atualizações

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -9,7 +9,9 @@
 
     <div class="informacao-pagina">
         <div class="contato-principal">
-            @component('site.layouts._components.form_contato')
+            @component('site.layouts._components.form_contato', ['class' => 'borda-preta'])
+                <p>A nossa equipe analisará a sua mensagem e retornaremos o mais brevemente possível</p>
+                <p>Nosso tempo médio de resposta é de 48 hora.</p>
             @endcomponent
         </div>
     </div>
